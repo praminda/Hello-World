b477f24487dbbd21bab8a4d45984fad60814d9ea
yehancha <yehancha@github>
2012-12-01 09:47:42 +0530

    Initial commit
    new file: README
    
    new file:   .gitignore
    Added file types to ignore:
    *~
    *.class
    
    new file:   HelloWorld.java
    A simle java class for common HelloWorld program.

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+	private String hw;
+	
+	public HelloWorld() {
+		hw = "Hello World!";
+	}
+	
+	public HelloWorld(String hw) {
+		this.hw = hw;
+	}
+	
+	public void print() {
+		System.out.println(hw);
+	}
+	
+	public void setHW(String hw) {
+		this.hw = hw;
+	}
+	
+	public static void main(String[] args) {
+		HelloWorld hw = new HelloWorld();
+		hw.print();
+	}
+}
